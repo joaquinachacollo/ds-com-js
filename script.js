2b352bdc8e081d30b6db3bea767ff8ea9eb50a19
joaquin achacollo <joaquinachacollo@gmail.com>
2022-07-11 15:33:38 -0300

    desafio-js

---
+++ script.js
@@ -0,0 +1,41 @@
+//////////////////// EJERCICIO 1 /////////////////////
+// let numero1 = ""
+// let numero2 = ""
+// let resultado = ""
+// let condicion = ""
+
+// do {
+//     numero1 = parseInt(prompt("ingrese su primer numero:"))
+//     numero2 = parseInt(prompt("ingrese su segundo numero:"))
+//     resultado = numero1 + numero2
+//     alert("el resultado es " + resultado)
+//     condicion = prompt("¿quiere continuar sumando?")
+// } while (condicion != "no");
+
+// alert("fin del programa")
+
+
+//////////////////// EJERCICIO 2 /////////////////////
+// let texto = prompt("ingrese su texto:")
+// let valor = prompt("ingrese su numero:")
+// let union = texto + " " + valor
+// alert(union)
+// let pregunta = prompt("¿quiere seguir con el programa?")
+// while (pregunta != "no") {
+//     texto = prompt("ingrese su texto:")
+//     valor = prompt("ingrese su numero:")
+//     union = texto + " " + valor
+//     alert(union)
+//     pregunta = prompt("¿quiere seguir con el programa?")
+// }
+
+// alert("fin del programa")
+
+
+////////////////// ejercicio 3 ///////////////////////
+
+// let numero = prompt("ingrese la cantidad de veces que sea repetir el mensaje:")
+
+// for (let i = 0; i < numero; i++) {
+//     console.log("hola")
+// }
